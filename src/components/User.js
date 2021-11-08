ea720bc40dd57e614d0fbd7c05673a73e87543f5
Henry Salliss <henrysalliss@gmail.com>
2021-11-08 18:48:18 +0000

    created class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,13 @@
-import classes from './User.module.css';
+import classes from "./User.module.css";
+import { Component } from "react";
+
+class User extends Component {
+  constructor() {}
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
 
 const User = (props) => {
   return <li className={classes.user}>{props.name}</li>;
